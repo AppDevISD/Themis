78877f735331f5ad9ba0369de878915bf011836d
KyleCWLP <Kyle.Bolinger@cwlp.com>
2024-06-18 11:56:46 -0500

    Added Data Library

---
+++ DataLibrary/Factory.cs
@@ -0,0 +1,50 @@
+﻿using DataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DataLibrary
+{
+    public class Factory
+    {
+        public static Factory _Factory = null;
+        private static SqlConnection _cn = null;
+        public string errorMsg = "";
+
+        public static Factory Instance
+        {
+            get
+            {
+                if (_Factory == null)
+                {
+                    _Factory = new Factory();
+                }
+                return _Factory;
+            }
+        }
+
+        private Factory()
+        {
+            _cn = new SqlConnection();
+            _cn.Close();
+        }
+
+        public string GetUserTheme()
+        {
+            return Properties.Settings.Default.userTheme.ToString();
+        }
+
+        public void SetUserTheme(string theme)
+        {
+            Properties.Settings.Default["userTheme"] = theme;
+        }
+    }
+}
---
+++ DataLibrary/Utility.cs
@@ -0,0 +1,58 @@
+﻿using DataLibrary;
+using ISD.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DataLibrary
+{
+    public class Utility
+    {
+        private static Utility _Utility;
+        public static Utility Instance
+        {
+            get
+            {
+                if (_Utility == null)
+                {
+                    _Utility = new Utility();
+                }
+                return _Utility;
+            }
+        }
+        public ADUser AuthenticateUser()
+        {
+            bool spoofUser = false;
+            string strLoginID;
+            ADUser adu = new ADUser();
+            string[] strTemp = { "", "" };
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                switch (spoofUser)
+                {
+                    case true:
+                        strLoginID = "";
+                        adu = ISDFactory.Instance.GetUserInformationByLoginName(strLoginID);
+                        break;
+
+                    case false:
+                        strLoginID = HttpContext.Current.User.Identity.Name;
+                        if (strLoginID.Length > 0)
+                        {
+                            strTemp = strLoginID.Split('\\');
+                            strLoginID = strTemp[1];
+                        }
+                        else { }
+                        adu = ISDFactory.Instance.GetUserInformationByLoginName(strLoginID);
+                        break;
+
+                }
+
+            }
+            return adu;
+        }
+    }
+}
